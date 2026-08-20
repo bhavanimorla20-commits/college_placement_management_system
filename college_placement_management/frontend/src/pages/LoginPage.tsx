bae2c5c45b81243298f12b777b47a404abcb2a66
Morla Bhavani <bhavanimorla20@gmail.com>
2026-08-20 14:50:58 +0530

    Modified API URL

--- college_placement_management/frontend/src/pages/LoginPage.tsx
+++ college_placement_management/frontend/src/pages/LoginPage.tsx
@@ -21,7 +21,7 @@ export default function LoginPage({
 
     try {
       const response = await fetch(
-        "`${import.meta.env.VITE_API_URL}/login`",
+        `${import.meta.env.VITE_API_URL}/login`,
         {
           method: "POST",
           headers: {
