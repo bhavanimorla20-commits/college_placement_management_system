f5401570358937bb79b7d1388a19b1107f3368d2
Morla Bhavani <bhavanimorla20@gmail.com>
2026-08-20 16:22:51 +0530

    Fix CORS

--- college_placement_management/backend/main.py
+++ college_placement_management/backend/main.py
@@ -40,7 +40,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "http://localhost:3000",
+                   "https://college-placement-management-system-flax.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
