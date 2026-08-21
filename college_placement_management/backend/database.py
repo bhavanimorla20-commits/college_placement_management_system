ba89e4f83d62ce3b49a4310b99504e3c9031d924
Morla Bhavani <bhavanimorla20@gmail.com>
2026-08-21 11:14:08 +0530

    Modified database connection

--- college_placement_management/backend/database.py
+++ college_placement_management/backend/database.py
@@ -6,7 +6,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    connect_args={
+        "sslmode": "require"
+    }
+    )
 
 SessionLocal = sessionmaker(
     autocommit=False,
